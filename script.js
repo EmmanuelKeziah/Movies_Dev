f221d6ff8a4508f1e68defcc4ee5be83bdddf4a8
Emmanuel Keziah <keziahema@gmail.com>
2025-06-13 22:41:57 +0100

    Update files

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-( async function (){
-    try {
-        let url = "https://api.themoviedb.org/3/movie/popular?api_key=1ef33d0988889fd4f6c374211d20e38c";
+const fetchMoviesByPopularity = async function (){
+    let url = "https://api.themoviedb.org/3/movie/popular?api_key=1ef33d0988889fd4f6c374211d20e38c";
 
-        let allMovies = await fetch (url);
-        let allMoviesJson = await allMovies.json();
+    let allMovies = await fetch (url);
+    let allMoviesJson = await allMovies.json();
 
-        console.log(allMoviesJson); //the response from the API
+    // console.log(allMoviesJson); //Used to get the initial response from the API
 
-        let movieContainer = document.getElementById("movieList");
-        let newImg = document.createElement("img");
-        let newTitle = document.createElement("h2");
+    for (let i = 0; i < allMoviesJson.results.length; i++) {
+        let movieBox = document.createElement("div");
+        let movieImg = document.createElement("img");
+        let movieTitle = document.createElement("h2");
 
-        newDiv.appendChild(newImg);
-        newDiv.appendChild(newTitle);
+        let parentDiv = document.getElementById("movieList");
+
+
+
+        movieImg.src = "https://image.tmdb.org/t/p/w500" + allMoviesJson.results[i].poster_path;
+        movieTitle.innerText = allMoviesJson.results[i].title;
+
+        movieBox.appendChild(movieImg); //This attaches the image to the movie box div
+        movieBox.appendChild(movieTitle);
+        parentDiv.appendChild(movieBox);
+
+        // console.log(allMoviesJson.results[i].title);
+        // console.log(allMoviesJson.results);
+        // console.log(allMoviesJson.results[i].poster_path);
     }
-    catch (error) {
-        console.error("Error fetching movies:", error);
-    }
-})();
+
+    
+    
+}();
